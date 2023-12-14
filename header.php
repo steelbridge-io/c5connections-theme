a8a058e7a93a647c8c549db829e6d886f5b32254
steelbridge-io <chris@steelbridge.io>
2023-12-14 15:17:49 -0800

    Change site title tag from paragraph to heading in c5connections theme
    The site title tag in the header of c5connections theme has been changed from a paragraph (p) element to a heading (h1) element. This modification will aid in SEO and provide better document structure, while maintaining the visual presentation of the site title.

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			endif;
 			$c5connections_theme_description = get_bloginfo( 'description', 'display' );
